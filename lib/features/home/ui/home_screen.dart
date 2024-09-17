49abb1d470e5668282379f9a48fe8d3998356360
golibjon <golibtoramurodov@gmail.com>
2024-09-17 17:07:43 +0900

    das

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_application/features/home/blocs/general_bloc.dart';
 import 'package:flutter_application/features/home/blocs/general_events.dart';
 import 'package:flutter_application/features/home/blocs/general_statas.dart';
-import 'package:flutter_application/features/home/data/models/expanse_model.dart';
-import 'package:flutter_application/features/home/data/models/income.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatefulWidget {
   final String username;
@@ -18,126 +16,13 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    // Load general data when the screen is initialized
     context.read<GeneralBloc>().add(LoadGeneralEvent(widget.username));
-  }
-
-  void _showAddIncomeDialog() {
-    final TextEditingController _amountController = TextEditingController();
-    final TextEditingController _reasonController = TextEditingController();
-
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Add Income'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: _amountController,
-                decoration: const InputDecoration(labelText: 'Amount'),
-                keyboardType: TextInputType.number,
-              ),
-              TextField(
-                controller: _reasonController,
-                decoration: const InputDecoration(labelText: 'Reason'),
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                final amount = double.tryParse(_amountController.text);
-                final reason = _reasonController.text;
-
-                if (amount != null && reason.isNotEmpty) {
-                  context.read<GeneralBloc>().add(
-                    AddIncomeEvent(
-                      username: widget.username,
-                      amount: amount,
-                      reason: reason,
-                    ),
-                  );
-                  Navigator.of(context).pop();
-                }
-              },
-              child: const Text('Add'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void _showAddExpenseDialog() {
-    final TextEditingController _amountController = TextEditingController();
-    final TextEditingController _reasonController = TextEditingController();
-
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Add Expense'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: _amountController,
-                decoration: const InputDecoration(labelText: 'Amount'),
-                keyboardType: TextInputType.number,
-              ),
-              TextField(
-                controller: _reasonController,
-                decoration: const InputDecoration(labelText: 'Reason'),
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                final amount = double.tryParse(_amountController.text);
-                final reason = _reasonController.text;
-
-                if (amount != null && reason.isNotEmpty) {
-                  context.read<GeneralBloc>().add(
-                    AddExpenseEvent(
-                      username: widget.username,
-                      amount: amount,
-                      reason: reason,
-                    ),
-                  );
-                  Navigator.of(context).pop();
-                }
-              },
-              child: const Text('Add'),
-            ),
-          ],
-        );
-      },
-    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xff1C1C1E),
-      appBar: AppBar(
-        title: const Text('Home'),
-        backgroundColor: Colors.grey[900],
-      ),
       body: BlocBuilder<GeneralBloc, GeneralState>(
         builder: (context, state) {
           if (state is GeneralLoadingState) {
@@ -146,6 +31,7 @@ class _HomeScreenState extends State<HomeScreen> {
             );
           }
           if (state is GeneralErrorState) {
+            print(state.message);
             return Center(
               child: Text(
                 state.message,
@@ -154,83 +40,69 @@ class _HomeScreenState extends State<HomeScreen> {
             );
           }
           if (state is GeneralLoadedState) {
+            final generalData = state.general;
             return Padding(
               padding: const EdgeInsets.fromLTRB(20, 50, 20, 0),
               child: SingleChildScrollView(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Row(
+                    const Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        const Text(
+                        Text(
                           'Overview',
                           style: TextStyle(color: Colors.white, fontSize: 24),
                         ),
                         CircleAvatar(
-                          child: Icon(Icons.person, color: Colors.white),
-                          backgroundColor: Colors.grey[800],
+                          child: Icon(Icons.person),
                         ),
                       ],
                     ),
                     const SizedBox(height: 20),
                     Card(
                       color: Colors.grey[800],
-                      child: ListTile(
-                        title: const Text('Total Salary'),
-                        subtitle: Text(
-                          '\$${state.general.totalSalary.toStringAsFixed(2)}',
-                          style: const TextStyle(color: Colors.white),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Total Salary',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 18),
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              '${generalData.totalSalary}',
+                              style: const TextStyle(
+                                  color: Colors.white, fontSize: 22),
+                            ),
+                          ],
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        ElevatedButton(
-                          onPressed: _showAddIncomeDialog,
-                          child: const Text('Add Income'),
-                        ),
-                        ElevatedButton(
-                          onPressed: _showAddExpenseDialog,
-                          child: const Text('Add Expense'),
-                        ),
-                      ],
                     ),
                     const SizedBox(height: 20),
                     const Text(
                       'Expenses:',
                       style: TextStyle(color: Colors.white, fontSize: 18),
                     ),
-                    if (state.general.expanseList.isNotEmpty)
-                      for (var expanse in state.general.expanseList)
-                        Text(
-                          '- ${expanse.name}: \$${expanse.amount.toStringAsFixed(2)}',
-                          style:
-                              const TextStyle(color: Colors.white, fontSize: 16),
-                        )
-                    else
-                      const Text(
-                        'No expenses available',
-                        style: TextStyle(color: Colors.white, fontSize: 16),
+                    for (var expanse in generalData.expanseList)
+                      Text(
+                        '- ${expanse.name}: ${expanse.amount}',
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 16),
                       ),
-                    const SizedBox(height: 10),
+                    const SizedBox(height: 20),
                     const Text(
                       'Income:',
                       style: TextStyle(color: Colors.white, fontSize: 18),
                     ),
-                    if (state.general.incomeList.isNotEmpty)
-                      for (var income in state.general.incomeList)
-                        Text(
-                          '- ${income.name}: \$${income.amount.toStringAsFixed(2)}',
-                          style:
-                              const TextStyle(color: Colors.white, fontSize: 16),
-                        )
-                    else
-                      const Text(
-                        'No income available',
-                        style: TextStyle(color: Colors.white, fontSize: 16),
+                    for (var income in generalData.incomeList)
+                      Text(
+                        '- ${income.name}: ${income.amount}',
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 16),
                       ),
                   ],
                 ),
